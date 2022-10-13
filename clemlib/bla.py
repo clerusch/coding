3cd8abf45f525517635b348ff46e1d37a2c3e930
clemens <clemensrubenschumann@googlemail.com>
2022-10-13 16:55:08 +0200

    formatting

--- clemlib/bla.py
+++ clemlib/bla.py
@@ -1,30 +1,37 @@
 import numpy as np
 
+
 class ringMessage:
     def __init__(self, bit=0):
         self.content = np.array([bit])
-        self.show = lambda: print(f"Content:{self.content}\nSyndrome:{self.syndrome}")
+        self.show = lambda: print(
+            f"Content:{self.content} \nSyndrome:{self.syndrome}")
         self.syndrome = np.array([])
         self.corrected = np.array([0 for _ in range(5)])
+
     def encode(self, codelength=5):
-        self.content = np.array([self.content[0] for _ in range(codelength)])
+        self.content = np.array(
+            [self.content[0] for _ in range(codelength)])
         self.syndrome = np.array([0 for _ in range(codelength)])
+
     def noisify(self, noisiness=0.25):
         for i in range(len(self.content)):
             if np.random.random() < noisiness:
                 self.content[i] = (self.content[i] + 1) % 2
                 self.syndrome[i] = (self.syndrome[i] + 1) % 2
                 self.syndrome[i-1] = (self.syndrome[i-1] + 1) % 2
+
     def decode(self, codelength=5):
-        pcm = np.array([[1,1,0,0,0],
-                        [0,1,1,0,0],
-                        [0,0,1,1,0],
-                        [0,0,0,1,1],
-                        [1,0,0,0,1]])
+        pcm = np.array([[1, 1, 0, 0, 0],
+                        [0, 1, 1, 0, 0],
+                        [0, 0, 1, 1, 0],
+                        [0, 0, 0, 1, 1],
+                        [1, 0, 0, 0, 1]])
         print(pcm @ self.content.T, self.syndrome.T)
-        print(pcm.T@self.syndrome.T) 
+        print(pcm.T@self.syndrome.T)
         # for i in range(len(self.content)):
         #     self.content[i] = (self.content[i]+1)%2
+
 
 word = ringMessage()
 word.show()
--- tester.py
+++ tester.py
@@ -50,8 +50,11 @@ def plotter(howgood):
     """
     """x = np.outer(np.linspace(-2, 2, 10), np.ones(10))
     y = x.copy().T
-    z = fidelity(x,y)
-    fig = plt.figure() 
+    z = fidelity(x,y) idk if I even have to do this
+    fig = plt.figure() also whent f are they going to  be going away I need peace & quiet god do I hope I can hold on until 04/11
+    but also wow what a good move of me to go to paris next week I would not be making it without a break
+    please just go shopping instead of weirdly shouting at each other you dumb fuckers
+    also I probably should just close my door
 
     syntax for 3-D plotting
     ax = plt.axes(projection ='3d')
