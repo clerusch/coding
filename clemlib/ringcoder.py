be6b8485bc2382db4932c1a9ed61feef490d0281
clemens <clemensrubenschumann@googlemail.com>
2022-10-14 11:21:43 +0200

    wrong plots now

--- clemlib/ringcoder.py
+++ clemlib/ringcoder.py
@@ -1,35 +1,68 @@
 import numpy as np
-## lets decode a logical 0 or 1
+from numpy.linalg import inv
+import matplotlib.pyplot as plt
 
-m = np.array([[1,1,0,0,0],[0,1,1,0,0],[0,0,1,1,0],[0,0,0,1,1]])
-actual = np.array([0,1,0,0,0])
-#print(m@actual)
+class ringMessage:
+    def __init__(self, bit=0, name=None, number=0):
+        self.number = number
+        self.name = name
+        self.content = np.array([bit])
+        self.show = lambda: print(
+            f"Content:{self.content} \nSyndrome:{self.syndrome}"+
+            "\n"+self.stage)
+        self.syndrome = np.array([])
+        self.stage = "Oh no, how will I survive a physical error?"
 
-def encoder(bit):
-    return np.array([[bit] for _ in range(5)])
+    def encode(self, codelength=5):
+        self.content = np.array(
+            [self.content[0] for _ in range(codelength)])
+        self.syndrome = np.array([0 for _ in range(codelength)])
+        self.stage = "I am encoded"
 
-def noisify(message, noisiness=0.3):
-    syndrome = np.array([[0],[0],[0],[0],[0]])
-    for count, bit in enumerate(message):
-        if np.random.rand() < noisiness:
-            bit = (bit+1)%2
-            syndrome[count-1][0] += 1
-            if count < len(syndrome):
-                syndrome[count+1][0] += 1
-            else: 
-                syndrome[0][0] += 1
-    for a in syndrome:
-            a[0] = a[0] % 2
-    
-    return message, syndrome
+    def noisify(self, noisiness=0.25):
+        for i in range(len(self.content)):
+            if np.random.random() < noisiness:
+                self.content[i] = (self.content[i] + 1) % 2
+                self.syndrome[i] = (self.syndrome[i] + 1) % 2
+                self.syndrome[i-1] = (self.syndrome[i-1] + 1) % 2
+        self.stage = "I have been subjected to noise :("
 
-def decoder(message,syndrome):
-    pcm = np.array([[1,1,0,0,0],[0,1,1,0,0],[0,0,1,1,0],[0,0,0,1,1],[1,0,0,0,1]])
-    guessed_error = pcm.__invert__ @ syndrome
-    answer = message + guessed_error
-    for a in answer:
-        a = a%2
-    return answer
-test, syndrome = noisify(encoder(0)) 
-print(test, syndrome)
-#print(decoder(test, syndrome))
+    def decode(self, codelength=5):
+        pcm = np.array([[0 for _ in range(codelength)] for _ in range(codelength)])
+        for i in range(codelength):
+            pcm[i,i] = 1; pcm[i,i-codelength+1] = 1
+        #this here  is what pcm looks like for codelength 5
+        pcb = np.array([[1, 1, 0, 0, 0],
+                        [0, 1, 1, 0, 0],
+                        [0, 0, 1, 1, 0],
+                        [0, 0, 0, 1, 1],
+                        [1, 0, 0, 0, 1]])
+        # Ring pcm matrices are square and have full rank, so they're invertible
+        noise = np.array((inv(pcm)@self.syndrome.T).T, dtype=int)
+        self.content = (self.content + noise) % 2
+        self.stage = "Hopefully my corrections were right UwU"
+        
+amount = 100000
+smoothness = 1000
+lErrorCount = []
+pErrorCount = []
+words = [ringMessage(number=i) for i in range(amount)]
+for i in range(amount):
+    noisiprob = i/(amount)
+    pErrorCount.append(noisiprob)
+    words[i].encode(5)
+    real = words[i].content
+    words[i].noisify(noisiprob)
+    words[i].decode(5)
+    if np.array_equal(real,words[i].content):
+        lErrorCount.append(0)
+    else: lErrorCount.append(1)
+
+logical_error_rate = [(sum(lErrorCount[i:i+smoothness])/smoothness) \
+    for i in range(0,amount-smoothness,smoothness)] 
+physical_error_rate = [(sum(pErrorCount[i:i+smoothness])/smoothness) \
+    for i in range(0,amount-smoothness,smoothness)]
+
+plt.plot(physical_error_rate,logical_error_rate)
+plt.savefig(f"Samplesize={amount}smoothness={smoothness}")
+plt.show()
