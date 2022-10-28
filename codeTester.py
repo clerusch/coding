4978162d0f3e45a2da0e0b4d957406f9a5939dc5
clemens <clemensrubenschumann@googlemail.com>
2022-10-28 11:35:43 +0200

    added modularity for different systems/pcms to lercalc

--- codeTester.py
+++ codeTester.py
@@ -7,6 +7,9 @@ This is to test decoding algorithms for repetition based codes
 """
 
 def genRepPCM(distance):
+    """
+    Generates a repetition code parity-check-matrix
+    """
     nq = distance   # number of qubits
     na = nq - 1     # number of ancillas
     pcm = np.array([[0 for _ in range(nq)] for _ in range(na)])
@@ -15,8 +18,20 @@ def genRepPCM(distance):
         pcm[i][(i+1) % nq] = 1
     return pcm
 
-def lercalc(nr = 100, dist = 5, per = 0.3):
-    pcm = genRepPCM(dist)
+def genRingPCM(distance):
+    """
+    Generates a ring code parity-check-matrix of specific distance
+    """
+    pcm=np.eye(distance)
+    for i in range(distance):
+        pcm[i][(i+1)%distance] = 1
+    return pcm
+
+def lerCalc(pcmType, nr = 100, dist = 5, per = 0.3):
+    """
+    Calculates a logical error rate of dist code assuming specific physical error rate
+    """
+    pcm = pcmType(dist)
     matching = Matching(pcm)
     nle = 0
     for _ in range(nr):
@@ -28,7 +43,7 @@ def lercalc(nr = 100, dist = 5, per = 0.3):
             nle += 1
     return nle/nr
 
-def main(dists = [3,5,9,29,35]):
+def main(dists = [5,51]):
     """
     For simplicity's sake this will test on a zero message
     """
@@ -36,9 +51,15 @@ def main(dists = [3,5,9,29,35]):
         ler = []
         per = [i/10 for i in range(10)]
         for p in per:
-            new_ler = lercalc(1000, d, p)
+            new_ler = lerCalc(genRepPCM,1000, d, p)
             ler.append(new_ler)
-        plt.plot(per, ler, label=d)
+        plt.plot(per, ler, label=f"rep {d}")
+        ler = []
+        per = [i/10 for i in range(10)]
+        for p in per:
+            new_ler = lerCalc(genRingPCM,1000, d, p)
+            ler.append(new_ler)
+        plt.plot(per, ler, label=f"ring {d}")
     plt.legend()
     plt.show()
 
