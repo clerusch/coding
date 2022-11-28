7c9e213efa2adf096a5609e5f4b7a4372d6bf790
clemens <clemensrubenschumann@googlemail.com>
2022-11-28 13:44:13 -0300

    added stuff on the clifford group

--- conceptTinkering/playground/play.py
+++ conceptTinkering/playground/play.py
@@ -1,9 +1,1 @@
-import numpy as np
-dist = 5
-nq = dist       # number of qubits
-na = nq - 1     # number of ancillas
-pcm = np.array([[0 for _ in range(nq)] for _ in range(na)])
-for i in range(na):
-    pcm[i][i] = 1
-    pcm[i][(i+1) % nq] = 1
-print(pcm) 
+
