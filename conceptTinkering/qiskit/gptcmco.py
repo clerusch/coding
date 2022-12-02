e523e0e55a4882b370ffc8b80cd99dbf9368eb62
clemens <clemensrubenschumann@googlemail.com>
2022-12-02 10:37:29 -0300

    Here's what I got so far, didn't even finish the circuit analysis but revamped structuring of the algebra intro

---
+++ conceptTinkering/qiskit/gptcmco.py
@@ -0,0 +1,24 @@
+#import qiskit
+from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
+from qiskit import execute, Aer
+
+#create quantum and classical registers
+q = QuantumRegister(2)
+c = ClassicalRegister(2)
+
+#create a quantum circuit
+qc = QuantumCircuit(q, c)
+
+#add a CNOT gate
+qc.cx(q[0], q[1])
+
+#add ancilla measurements
+qc.measure(q[0], c[0])
+qc.measure(q[1], c[1])
+
+#add conditional X and Z gates
+qc.x(q[1]).c_if(c, 1)
+qc.z(q[1]).c_if(c, 2)
+
+#draw the circuit
+qc.draw(output='mpl')
